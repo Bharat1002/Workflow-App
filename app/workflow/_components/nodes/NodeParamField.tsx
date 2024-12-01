9bf1587c0bf7f8f6899cd033fd87c9b8bca51916
Bharat1002 <bharatjc1002@gmail.com>
2024-12-01 21:53:43 +0530

    update workflow feature add

--- app/workflow/_components/nodes/NodeParamField.tsx
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -1,12 +1,42 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
 import { TaskParam, TaskParamType } from "@/types/task";
+import StringParam from "@/app/workflow/_components/nodes/param/StringParam";
+import { useReactFlow } from "@xyflow/react";
+import { AppNode } from "@/types/appNode";
+import { useCallback } from "react";
 
-function NodeParamField({ param }: { param: TaskParam }) {
+function NodeParamField({
+  param,
+  nodeId,
+}: {
+  param: TaskParam;
+  nodeId: string;
+}) {
+  const { updateNodeData, getNode } = useReactFlow();
+  const node = getNode(nodeId) as AppNode;
+  const value = node?.data.inputs?.[param.name];
+
+  const updateNodeParamValue = useCallback(
+    (newValue: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data.inputs,
+          [param.name]: newValue,
+        },
+      });
+    },
+    [nodeId, updateNodeData, param.name, node?.data.inputs]
+  );
   switch (param.type) {
     case TaskParamType.STRING:
-      return <Input />;
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      );
     default:
       return (
         <div className="w-full">
