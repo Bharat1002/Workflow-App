12d849402ff111bc3fe7f81aae82d57ada88223d
Bharat1002 <bharatjc1002@gmail.com>
2025-02-19 19:50:07 +0530

    feat: scroll to element task

--- app/workflow/_components/TaskMenu.tsx
+++ app/workflow/_components/TaskMenu.tsx
@@ -6,9 +6,11 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { TaskType } from "@/types/task";
+import { CoinsIcon } from "lucide-react";
 
 export default function TaskMenu() {
   return (
@@ -32,6 +34,7 @@ export default function TaskMenu() {
             <TaskMenuBtn taskType={TaskType.NAVIGATE_URL} />
             <TaskMenuBtn taskType={TaskType.FILL_INPUT} />
             <TaskMenuBtn taskType={TaskType.CLICK_ELEMENT} />
+            <TaskMenuBtn taskType={TaskType.SCROLL_TO_ELEMENT} />
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="extraction">
@@ -87,8 +90,14 @@ function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
       draggable
       onDragStart={(event) => onDragStart(event, taskType)}
     >
-      <task.icon size={20} />
-      {task.label}
+      <div className="flex gap-2">
+        <task.icon size={20} />
+        {task.label}
+      </div>
+      <Badge className="gap-2 flex items-center" variant={"outline"}>
+        <CoinsIcon size={16} />
+        {task.credits}
+      </Badge>
     </Button>
   );
 }
